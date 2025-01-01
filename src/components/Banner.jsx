78a22be520a50e4f387949290a9caf2497c7a007
Sahil Alamgir <sahil.alamgir@gmail.com>
2025-01-01 18:06:13 -0500

    Create banner/hero page with animation

---
+++ src/components/Banner.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta)
+
+    return () => clearInterval(ticker);
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setLoopNum(loopNum + 1);
+      setIsDeleting(false);
+      setDelta(500);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>{`Hi I'm Sahil, a `}<br></br><span className="wrap">{text}</span></h1>
+            <p>Loren ipsum is simply dummy text of the printing and typesetting industry. Loren ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <button onClick={() => console.log("Connect")}>Let's connect <ArrowRightCircle size={25}/></button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Image" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
